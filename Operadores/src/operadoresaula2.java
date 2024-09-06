dc101f5f0958153367cb1db08473b2e18a9bec14
BrunnstsDev <brunnstsdev@gmail.com>
2024-09-06 15:42:25 -0300

    pacote-operadores

---
+++ Operadores/src/operadoresaula2.java
@@ -0,0 +1,8 @@
+public class operadoresaula2 {
+    public static void main(String[] args) {
+
+        String nomeCompleto = "Bruno" + "Santos";
+        System.out.println(nomeCompleto);
+        
+    }
+}
