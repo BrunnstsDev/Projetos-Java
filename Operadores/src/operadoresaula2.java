d210fe77f4f78ae950e66604250bedb4322b7c1d
BrunnstsDev <brunnstsdev@gmail.com>
2024-09-06 15:58:37 -0300

    Update operadoresaula2.java

--- Operadores/src/operadoresaula2.java
+++ Operadores/src/operadoresaula2.java
@@ -3,6 +3,22 @@ public class operadoresaula2 {
 
         String nomeCompleto = "Bruno" + "Santos";
         System.out.println(nomeCompleto);
-        
+
+        //Atenção: O operador de adição (+), quando utilizados em variáveis do tipo texto, realizará a "concatenação de textos".
+
+        String concatenacao = ("?");
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
     }
 }
