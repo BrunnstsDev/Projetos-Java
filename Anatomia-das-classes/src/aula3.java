e372be293f0ec57475454ddef91d96771a012b84
BrunnstsDev <brunnstsdev@gmail.com>
2024-09-05 21:32:29 -0300

    aula 3 - anatomia das classes

---
+++ Anatomia-das-classes/src/aula3.java
@@ -0,0 +1,16 @@
+public class aula3 {
+
+public static void main(String[] args) {
+    
+    String primeiroNome = "Bruno";
+    String segundoNome = "Santos";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+}
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
